97b7367787576001092b605987c414b09eb816d1
Tpicoulegiusta <117988938+Tpicoulegiusta@users.noreply.github.com>
2023-05-10 16:11:46 +0200

    Add files via upload

---
+++ Minishell/minishell.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpicoule <tpicoule@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/10 16:04:39 by tpicoule          #+#    #+#             */
+/*   Updated: 2023/05/10 16:09:15 by tpicoule         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct s_lexer
+{
+	char *prompt;
+}	t_lexer;
+
+typedef struct s_parser
+{
+	char **
+}
+
+#endif
