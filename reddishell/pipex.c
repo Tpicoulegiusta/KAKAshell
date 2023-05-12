404b8da8216f344b3d80ab1129593f6dca540bda
Tpicoulegiusta <117988938+Tpicoulegiusta@users.noreply.github.com>
2023-05-12 11:04:44 +0200

    Add files via upload

---
+++ reddishell/pipex.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbulanad <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/10 11:53:46 by rbulanad          #+#    #+#             */
+/*   Updated: 2023/05/11 19:08:13 by rbulanad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+/*
+void	exec(t_data *data, char **envp)
+{
+	int	pid;
+	
+	pid = fork();
+	if (pid == 0)
+		execve(data->path, data->argexec, envp);
+	waitpid(pid, NULL, 0);
+	free_execve(data);
+}*/
